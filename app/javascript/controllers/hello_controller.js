3b6fbfcf7557f70ca65867acfb7905d04be7d5dc
crisandryj <crisandryj@gmail.com>
2022-11-10 09:08:33 -0500

    updated javascript controller

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = [ "items" ]
+  static values = { index: { type: Number, default: 1 } }
+
+  indexValueChanged() {
+  this.showCurrentItem()
+  }
+
+  next() {
+  this.indexValue++
+  }
+
+  showCurrentItem() {
+    this.itemTargets.forEach((element, index) => {
+      element.hidden = index !== this.indexValue
+    })
+
   connect() {
     this.element.textContent = "Hello World!"
   }
+
 }
